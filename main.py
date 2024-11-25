e945caab6805a04d824bd65385699891a347eaea
ameliakaplonska <amelia.kaplonska@student.wat.edu.pl>
2024-11-25 13:42:08 +0100

    chore: add new users

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ users: list =[
     {'name':'Szymon z wąsem','posts':5,'city':'Inowrocław'},
     {'name':'Szymon','posts':7,'city':'Białograd'},
     {'name':'Patryk','posts':9,'city':'Łódź'},
+    {'name':'Karolina','posts':12,'city':'Płock'},
+    {'name':'Julia','posts':7,'city':'Zamość'},
+    {'name':'Aleksandra','posts':2,'city':'Radom'},
+    {'name':'Patrycja','posts':5,'city':'Lublin'},
+    {'name':'Patrycja','posts':12,'city':'Warszawa'},
+
+
 ]
 
 # TODO please update users list
