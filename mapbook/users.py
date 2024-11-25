6de14b730159f60bea2c200861c7d973e4980db8
ameliakaplonska <amelia.kaplonska@student.wat.edu.pl>
2024-11-25 14:55:24 +0100

    chore: formating

--- mapbook/users.py
+++ mapbook/users.py
@@ -9,4 +9,10 @@ users: list =[
     {'name':'Aleksandra','posts':2,'city':'Radom'},
     {'name':'Patrycja','posts':5,'city':'Lublin'},
     {'name':'Patrycja','posts':12,'city':'Warszawa'},
+    {'name':'Dominik','posts':33,'city':'Siedlce'},
+    {'name':'Michał','posts':1,'city':'Opole'},
+    {'name':'Żerom','posts':24,'city':'Rzeszów'},
+    {'name':'Amelia','posts':55,'city':'Toruń'},
+    {'name':'Patrycja','posts':66,'city':'Wrocław'},
+
 ]
