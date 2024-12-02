028df6539d8842eaf8c6ce7059e23072ddf88499
kingastaszynska <kinga.staszynska@student.wat.edu.pl>
2024-12-02 14:53:04 +0100

    feat: add update user function

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -13,11 +13,21 @@ def add_user(userlist:list) ->None:
     new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
     userlist.append(new_user)
 
-
 def remove_user(userlist:list)->None:
     user_to_find:str=input('podaj imię do usunięcia: ')
-    for user in users:
+    for user in userlist:
         if user['name'] == user_to_find:
             print(f'usuwam: {user}')
             userlist.remove(user)
+
+def update_user(userlist:list)->None:
+    user_to_find:str=input('podaj imię użytkownika do aktualizacji: ')
+    for user in userlist:
+        if user['name'] == user_to_find:
+            new_name: str = input("proszę podać nowe imię ")
+            new_posts: int = int(input("proszę podać nową ilość postów "))
+            new_city: str = (input("proszę podać nowe miasto "))
+            user['name'] = new_name
+            user['posts'] = new_posts
+            user['city'] = new_city
 
