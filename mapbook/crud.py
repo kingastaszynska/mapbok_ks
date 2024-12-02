ba6589fd128c198f976773d29b76180d90347315
kingastaszynska <kinga.staszynska@student.wat.edu.pl>
2024-12-02 12:58:08 +0100

    feat: add create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -5,3 +5,10 @@ def hello(user:str)->None:
 def read_users(users:list)->None:
         for user in users[1:]:
             print(f'Twój znajomy {user['name']}, z miejscowości {user['city']} opublikował {user['posts']} postów.')
+
+def add_user(userlist:list) ->None:
+    new_name:str=input("proszę podać imię nowego znajomego ")
+    new_posts:int=int(input("proszę podać ilość postów "))
+    new_city:str=(input("proszę podać miasto "))
+    new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
+    userlist.append(new_user)
