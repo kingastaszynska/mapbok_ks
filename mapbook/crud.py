fa8acb7383639052aa87fa8b0178be85b2074597
kingastaszynska <kinga.staszynska@student.wat.edu.pl>
2024-12-02 14:33:43 +0100

    feat: add remove user function

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -12,3 +12,12 @@ def add_user(userlist:list) ->None:
     new_city:str=(input("proszę podać miasto "))
     new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
     userlist.append(new_user)
+
+
+def remove_user(userlist:list)->None:
+    user_to_find:str=input('podaj imię do usunięcia: ')
+    for user in users:
+        if user['name'] == user_to_find:
+            print(f'usuwam: {user}')
+            userlist.remove(user)
+
