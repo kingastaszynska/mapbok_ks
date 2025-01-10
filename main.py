3f1583a6d08a913bb30efd1ef4e08eff6a6bf384
kingastaszynska <kinga.staszynska@student.wat.edu.pl>
2025-01-10 10:14:07 +0100

    feat: crud

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def show_users():
     for idx, user in enumerate (users):
         listbox_lista_obiektow.insert(idx, f'{user.imie} {user.nazwisko} {user.postow} {user.lokalizacja}')
 
+
 def add_user()->None:
     name=entry_imie.get()
     surname=entry_nazwisko.get()
@@ -41,11 +42,43 @@ def add_user()->None:
     entry_imie.focus()
 
 
+
 def delete_user()->None:
     i=listbox_lista_obiektow.index(ACTIVE)
     print(i)
     users.pop(i)
     show_users()
+
+
+def edit_user()->None:
+    i=listbox_lista_obiektow.index(ACTIVE)
+    entry_imie.insert(0, users[i].imie)
+    entry_nazwisko.insert(0, users[i].nazwisko)
+    entry_liczba_postow.insert(0, users[i].postow)
+    entry_lokalizacja.insert(0, users[i].lokalizacja)
+    button_dodaj_obiekty.config(text='Zapisz zmiany', command=lambda: update_user(i))
+
+
+def update_user(i)->None:
+    users[i].imie=entry_imie.get()
+    users[i].nazwisko=entry_nazwisko.get()
+    users[i].postow=entry_liczba_postow.get()
+    users[i].lokalizacja=entry_lokalizacja.get()
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_liczba_postow.delete(0, END)
+    entry_lokalizacja.delete(0, END)
+    entry_imie.focus()
+    button_dodaj_obiekty.config(text = 'Dodaj obiekty',command = add_user)
+    show_users()
+
+
+def show_user_details()->None:
+    i=listbox_lista_obiektow.index(ACTIVE)
+    label_szczegoly_imie_wartosc.config(text=users[i].imie)
+    label_szczegoly_nazwisko_wartosc.config(text=users[i].nazwisko)
+    label_szczegoly_posty_wartosc.config(text=users[i].postow)
+    label_szczegoly_lokalizacja_wartosc.config(text=users[i].lokalizacja)
 
 
 root=Tk()
@@ -69,11 +102,11 @@ label_lista_obiektow = Label(ramka_lista_obiektow, text="Lista obiektów:")
 label_lista_obiektow.grid(row=0, column=0, columnspan=3)
 listbox_lista_obiektow=Listbox(ramka_lista_obiektow, width=50)
 listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
-button_pokaz_szczegoly = Button(ramka_lista_obiektow, text = 'Pokaż szczegóły', command = lambda: print('aaa'))
+button_pokaz_szczegoly = Button(ramka_lista_obiektow, text = 'Pokaż szczegóły', command = show_user_details)
 button_pokaz_szczegoly.grid(row=2, column=0)
 button_usun_obiekt = Button(ramka_lista_obiektow, text = 'Usuń obiekt', command=delete_user)
 button_usun_obiekt.grid(row=2, column=1)
-button_edytuj_obiekt = Button(ramka_lista_obiektow, text = 'Edytuj obiekt')
+button_edytuj_obiekt = Button(ramka_lista_obiektow, text = 'Edytuj obiekt', command=edit_user)
 button_edytuj_obiekt.grid(row=2, column=2)
 
 #obiekty wewnątrz formularza
